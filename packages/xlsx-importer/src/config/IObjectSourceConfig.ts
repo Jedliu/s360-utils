a0d476bcafafc42d656a79573b080154fd641fd7
Jedliu <jedliu@gmail.com>
2022-12-09 13:06:04 +0800

    remove deprecated codes

--- packages/xlsx-importer/src/config/IObjectSourceConfig.ts
+++ packages/xlsx-importer/src/config/IObjectSourceConfig.ts
@@ -2,9 +2,5 @@ import { IFieldConfig } from './IFieldConfig';
 import { ISourceConfig } from './ISourceConfig';
 
 export interface IObjectSourceConfig extends ISourceConfig {
-    fields: IFieldConfig[];
+  fields: IFieldConfig[];
 }
-
-/** @deprecated Default exports will be removed in January 2021. Please to use brackets (`{ IObjectSourceConfig }`). */
-// tslint:disable-next-line:no-empty-interface
-export default type IObjectSourceConfigLegacy = IObjectSourceConfig
--- packages/xlsx-importer/src/config/ISourceConfig.ts
+++ packages/xlsx-importer/src/config/ISourceConfig.ts
@@ -1,10 +1,6 @@
 import { ImportType } from './ImportType';
 
 export interface ISourceConfig {
-    type?: ImportType | string;
-    worksheet: string;
+  type?: ImportType | string;
+  worksheet: string;
 }
-
-/** @deprecated Default exports will be removed in January 2021. Please to use brackets (`{ ISourceConfig }`). */
-// tslint:disable-next-line:no-empty-interface
-export default type ISourceConfigLegacy = ISourceConfig
