ac83a48abe94d434a978402bb3a5007932d7caa8
Jedliu <jedliu@gmail.com>
2022-12-09 19:21:26 +0800

    remove deprecated codes

--- packages/xlsx-importer/src/Importer.ts
+++ packages/xlsx-importer/src/Importer.ts
@@ -3,7 +3,6 @@ import { Workbook } from '@s360-utils/exceljs';
 import { IMPORT_TYPE_DEFAULT, ImportType } from './config/ImportType';
 import { ISourceConfig } from './config/ISourceConfig';
 import { IImporter } from './IImporter';
-import IImporterLegacy from './IImporter';
 import { getStrategyByType } from './strategies';
 
 export class Importer implements IImporter {
@@ -12,7 +11,7 @@ export class Importer implements IImporter {
   public getAllItems<T>(config: ISourceConfig): T[] {
     const { worksheet } = config;
     const type = (config.type as ImportType) || IMPORT_TYPE_DEFAULT;
-    const ws = this.wb.getWorksheet(worksheet);
+    const ws = this.wb.getWorksheet(worksheet ?? 1);
 
     return getStrategyByType(type)(config, ws);
   }
@@ -20,17 +19,5 @@ export class Importer implements IImporter {
   public getFirstItem<T>(config: ISourceConfig): T {
     const wsData: T[] = this.getAllItems(config);
     return wsData[0];
-  }
-}
-
-/** @deprecated Default exports will be removed in January 2021. Please to use brackets (`{ Importer }`). */
-// tslint:disable-next-line:no-empty-interface max-classes-per-file
-export default class ImporterLegacy
-  extends Importer
-  implements IImporterLegacy
-{
-  /** @deprecated Refactoring performed, please to use `import { Importer }` and rename to camelCase version `getAllItems`. */
-  public GetAllItems<T>(config: ISourceConfig): T[] {
-    return this.getAllItems<T>(config);
   }
 }
--- packages/xlsx-importer/src/ImporterFactory.ts
+++ packages/xlsx-importer/src/ImporterFactory.ts
@@ -1,7 +1,7 @@
 import { Buffer, Workbook } from '@s360-utils/exceljs';
 
 import { IImporter } from './IImporter';
-import ImporterLegacy, { Importer } from './Importer';
+import { Importer } from './Importer';
 
 export class ImporterFactory {
   public async from(path: string): Promise<IImporter> {
@@ -14,17 +14,5 @@ export class ImporterFactory {
     const wb = new Workbook();
     await wb.xlsx.load(buffer);
     return new Importer(wb);
-  }
-}
-
-/** @deprecated Default exports will be removed in January 2021. Please to use brackets (`{ ImporterFactory }`). */
-// tslint:disable-next-line:no-empty-interface max-classes-per-file
-export default class ImporterFactoryLegacy extends ImporterFactory {
-  /** @deprecated Refactoring performed, please to use `import { ImporterFactory }` and rename to camelCase version `from`. */
-  public async From(path: string): Promise<ImporterLegacy> {
-    const wb = new Workbook();
-    await wb.xlsx.readFile(path);
-
-    return new ImporterLegacy(wb);
   }
 }
